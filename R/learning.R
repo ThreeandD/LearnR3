a7967aea7fb72367c641b35861e1907e5d522042
Mikkel Lundbech <mikkellundbech@gmail.com>
2023-04-26 09:55:44 +0200

    upload learning

---
+++ R/learning.R
@@ -0,0 +1,20 @@
+# Basics of R -------------------------------------------------------------
+
+
+colnames(airquality)
+
+str(airquality)
+
+summary(airquality)
+
+1 + 1
+
+mean(2:6)
+
+# Loading packages --------------------------------------------------------
+
+library(tidyverse)
+
+# This will be used for testing out GI
+
+# Here is an example of a conflict.
